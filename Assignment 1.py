77ede95ab051e2c88c99315d95f50d596db41fac
WangJ0347 <wangpham07@gmail.com>
2022-03-11 20:29:12 -0500

    Put codes in function and run in __main__

--- Assignment 1.py
+++ Assignment 1.py
@@ -1,12 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(-2*np.pi,2*np.pi,100)
-sin = np.sin(x)
-cos = np.cos(x)
 
-#plot 1 period of sin and cos on the same axes
-plt.plot(x,sin, label = 'sin')
-plt.plot(x,cos, label = 'cos')
-plt.legend()
-plt.show()
+def sin_cos(x:float):
+    """
+    :param x: Interval of period
+    :return: Sin and cos plot
+    """
+    sin = np.sin(x)
+    cos = np.cos(x)
+
+    # plot 1 period of sin and cos on the same axes
+    fig, ax = plt.subplots()
+    ax.plot(x, sin, label='sin')
+    ax.plot(x, cos, label='cos')
+    plt.axhline(y=0, color='k', linestyle='-')
+    ax.set_xlim(0, 2*np.pi)
+    ax.set_title('One period of sin and cos')
+    plt.legend()
+    plt.show()
+
+if __name__ == "__main__":
+    x = np.linspace(0, 2 * np.pi, 1000)
+    sin_cos(x)
